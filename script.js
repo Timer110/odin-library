fa940e7455d1bf159fbce8796ba5264fe3b7350c
Timer110 <121953691+Timer110@users.noreply.github.com>
2025-10-29 11:48:55 +0100

    created first visual Design and displayBooks function

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+// DOM Elements
+const bookshelf = document.querySelector(".bookshelf");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputRead = document.querySelector("#read");
+const inputButtonAddBook = document.querySelector("#addBook");
+
+// Variables
 const myLibrary = [];
 
-function Book (title, author, pages, read) {
+
+// Functions
+function Book (title, author, pages, read, id) {
     if (!new.target) {
         throw new Error("You must use the 'new' Operator to call the constructor-Function");
     }
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.id = id;
     if (read == true) {
         this.read = "read";
     }
@@ -19,11 +31,30 @@ function Book (title, author, pages, read) {
 }
 
 function addBookToLibrary (title, author, pages, read) {
-    
+    let tempId = crypto.randomUUID();
+    let newBook = new Book(title, author, pages, read, tempId)
+    myLibrary.push(newBook);
 }
 
-let hobbit = new Book("The Hobbit", "Author", 324, true);
-let homework = new Book("Homework", "Me", 24, false)
+function displayBooks () {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        card.textContent = myLibrary[i].title;
+        bookshelf.appendChild(card);
+    }
+}
 
-hobbit.info();
-homework.info();
+
+// Operands
+
+displayBooks();
+
+
+// Debug
+addBookToLibrary("The Hobbit", "J. R. R. Tolkien", 420, true);
+addBookToLibrary("Homework", "Me", 24, false);
+addBookToLibrary("Prüfungsamt", "IU University", 23, true);
+console.log(myLibrary);
+
+
